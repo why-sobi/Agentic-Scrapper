340d7050d6bad58b1b23cd76f442c9b58ae8df9d
ssgali <alirauf660@gmail.com>
2025-08-20 18:51:12 +0500

    Scrapper Initialized, just need to scrape details now

--- scrappers/olx_scrappers.py
+++ scrappers/olx_scrappers.py
@@ -3,7 +3,18 @@ import sys
 
 WEBSITE_URL = "https://www.olx.com.pk"
 
-def run(input_query: str, num_of_products: int) -> list[dict]:
+class Product():
+    def __init__(self,name,link,):
+        self.name = name
+        self.link = link
+
+    def get_product(self):
+        pass
+
+def scrape_url(link: str):
+    pass
+
+def run(input_query: str, num_of_products: int = 50) -> list[dict]:
 
     with sync_playwright() as p:
         # Launch a browser
@@ -20,27 +31,27 @@ def run(input_query: str, num_of_products: int) -> list[dict]:
 
         while(True):
             # Get the items list using class name
-            get_list_items = page.locator(".ec65250d li").all()
-            
+            ul = page.locator("xpath=/html/body/div/div[1]/header[2]/div/div/div/div[2]/div[2]/div[2]/div/div/div[3]/ul")
+            get_list_items = ul.locator('xpath=/li').all()
             # Loop through Items and extract link
             for item in get_list_items:
-                print("this is item",item)
                 link = item.locator("a").first          # Get the First <a> tag  
                 href = link.get_attribute("href")       # Get the First href for url
-                print(link,href)
                 if "https://www.olx.com.pk" in href:    # Filter out adds by using their URL
                     continue
                 else:
                     full_link = "https://www.olx.com.pk" + href
                     product_links.add(full_link)
-            if(len(product_links) >= num_of_products):
-                break
+                if(len(product_links) >= num_of_products):
+                    browser.close()
+                    return product_links
+            
             # If the length is not yet fulfilled, move towards the next page
             page_number += 1
-            page.goto(url + f"?page={page_number}", timeout=4000)
-            
-        browser.close()
-        return product_links
+            page.goto(url + f"?page={page_number}")
 
 if __name__ == "__main__":
-    print(run(sys.argv[1],int(sys.argv[2])))
+    if len(sys.argv) > 2:
+        print(run(sys.argv[1],int(sys.argv[2])))
+    else:
+        print(run(sys.argv[1]))
